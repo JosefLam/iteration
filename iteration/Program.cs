322df52b803b5afe702d4901ea78ae39024832f0
Josef Lambley <17LamblJ@fulford.york.sch.uk>
2022-09-12 14:33:24 +0100

    Add project files.

---
+++ iteration/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace iteration
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+    }
+}
